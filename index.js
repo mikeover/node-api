88ab54549af50dbb5443486ccd1b41fca4a188b0
Mike Over <mike.over@teamsnap.com>
2017-11-03 13:37:42 -0400

    Create initial server app

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Main starting point of application
+const express = require('express');
+const http = require('http'); // native node library for working with http requests
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+// App setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: "*/*" }));
+
+// Server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app); // create an HTTP server that receives requests and forwards them to app
+server.listen(port);
+console.log("Server listening on port " + port);
