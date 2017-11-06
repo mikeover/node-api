3bb2a579336d675dc9dca91afc8d8b3d3d251f0b
Mike Over <mike.over@teamsnap.com>
2017-11-06 09:31:40 -0500

    Create login method with passport to attempt to find user by ID in the jwt

--- services/passport.js
+++ services/passport.js
@@ -4,4 +4,23 @@ const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 
+// Setup options for JWT strategy
+const jwtOptions = {};
 
+// Create JWT strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+  // See if the user ID in the payload exists in DB
+  // If it does, call 'done' with that user,
+  // otherwise, call done without a user object
+  User.findById(payload.sub, function(err, user) {
+    if (err) { return done(err, false); }
+
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+});
+
+// Tell passport to use this strategy
